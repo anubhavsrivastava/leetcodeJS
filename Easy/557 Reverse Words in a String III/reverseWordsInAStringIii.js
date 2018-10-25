f856c2663579e8cd23fb918065c84eb82268f30e
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-10-25 21:25:55 +0530

    added #557 Reverse Words in a String III solution

---
+++ Easy/557 Reverse Words in a String III/reverseWordsInAStringIii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+	return s
+		.split(' ')
+		.map(w =>
+			w
+				.split('')
+				.reverse()
+				.join('')
+		)
+		.join(' ');
+};
