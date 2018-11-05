6bef56e40bba9f3b78dd1eb0569644830b71f8bd
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-11-05 22:29:16 +0530

    feat: modified scrapper to tackle SPA and SSR version of leetcode

--- Tools/scaffer/scrapper.js
+++ Tools/scaffer/scrapper.js
@@ -9,26 +9,58 @@ module.exports = {
 		const page = await browser.newPage();
 		await page.setViewport({ width: 1920, height: 926 });
 		await page.goto(leetCodeProblemURL);
-		let result = await page.waitFor('main').then(async () => {
-			// console.log('Main content loaded. Evaluating...');
-			let problemData = await page.evaluate(
-				async ({ UrlObj }) => {
-					let result = {};
-					result.difficulty = document.querySelectorAll('span[data-diff]')[0].getAttribute('data-diff');
-					result.completeProblemName = document.querySelectorAll(`a[href="${UrlObj.pathname}"]`)[0].innerText;
-					// problemName: "872.↵Leaf-Similar Trees"
-					let problemArr = result.completeProblemName.split('\n');
-					result.problemName = problemArr[1];
-					result.problemNumber = problemArr[0].split('.')[0];
-					result.question = document.querySelectorAll('div[role="tabpanel"] div div')[0].innerText;
-					return result;
-				},
-				{
-					UrlObj
-				}
-			);
-			return problemData;
-			// console.log(problemData);
+
+		let result;
+		await page.waitFor('body').then(async () => {
+			const isSPA = await page.evaluate(() => document.querySelector('#app') && document.querySelector('#app').length);
+			// This is SPA with either React/Vue/
+
+			if (isSPA) {
+				result = await page.waitFor('#main-container').then(async () => {
+					console.log('Main content loaded for SPA. Evaluating...');
+					let problemData = await page.evaluate(
+						async ({ UrlObj }) => {
+							let result = {};
+							result.difficulty = document.querySelectorAll('span[data-diff]')[0].getAttribute('data-diff');
+							result.completeProblemName = document.querySelectorAll(`a[href="${UrlObj.pathname}"]`)[0].innerText;
+							// problemName: "872.↵Leaf-Similar Trees"
+							let problemArr = result.completeProblemName.split('\n');
+							result.problemName = problemArr[1];
+							result.problemNumber = problemArr[0].split('.')[0];
+							result.question = document.querySelectorAll('div[role="tabpanel"] div div')[0].innerText;
+							return result;
+						},
+						{
+							UrlObj
+						}
+					);
+					return problemData;
+					// console.log(problemData);
+				});
+			} else {
+				result = await page.waitFor('.content-wrapper').then(async () => {
+					console.log('Main content loaded. Evaluating...');
+					let problemData = await page.evaluate(
+						async ({ UrlObj }) => {
+							let result = {};
+							result.difficulty = document.querySelectorAll('.difficulty-label')[0].innerText;
+							result.completeProblemName = document.querySelectorAll('.question-title .row h3')[0].innerText;
+							// problemName: "872. Leaf-Similar Trees"
+							let problemArr = result.completeProblemName.split('. ');
+							result.problemName = problemArr[1];
+							result.problemNumber = problemArr[0];
+							result.question = document.querySelectorAll('.question-content div div')[0].innerText;
+							return result;
+						},
+						{
+							UrlObj
+						}
+					);
+					// console.log(problemData);
+
+					return problemData;
+				});
+			}
 		});
 
 		browser.close();
