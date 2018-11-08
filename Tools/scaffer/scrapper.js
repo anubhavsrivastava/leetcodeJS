78c4b43386796fc6168f26fbb4c28f69b7076dfb
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-11-08 18:27:25 +0530

    fix: modified scrapper

--- Tools/scaffer/scrapper.js
+++ Tools/scaffer/scrapper.js
@@ -12,22 +12,36 @@ module.exports = {
 
 		let result;
 		await page.waitFor('body').then(async () => {
-			const isSPA = await page.evaluate(() => document.querySelector('#app') && document.querySelector('#app').length);
+			const isSPA = await page.evaluate(() => document.querySelectorAll('#app') && document.querySelectorAll('#app').length);
 			// This is SPA with either React/Vue/
-
+			console.log();
 			if (isSPA) {
-				result = await page.waitFor('#main-container').then(async () => {
+				result = await page.waitFor('#main-container div[diff]').then(async () => {
 					console.log('Main content loaded for SPA. Evaluating...');
 					let problemData = await page.evaluate(
 						async ({ UrlObj }) => {
 							let result = {};
-							result.difficulty = document.querySelectorAll('span[data-diff]')[0].getAttribute('data-diff');
-							result.completeProblemName = document.querySelectorAll(`a[href="${UrlObj.pathname}"]`)[0].innerText;
+							let order1Difficulty = document.querySelectorAll('div[diff]');
+
+							if (order1Difficulty.length) {
+								result.difficulty = order1Difficulty[0].getAttribute('diff');
+							} else {
+								result.difficulty = document.querySelectorAll('span[data-diff]')[0].getAttribute('data-diff');
+							}
+
+							let order1Description = document.querySelectorAll("div[class^='description'] >div");
+							if (order1Description.length) {
+								result.completeProblemName = order1Description[0].firstElementChild.innerText;
+								result.question = order1Description[1].innerText;
+							} else {
+								result.completeProblemName = document.querySelectorAll(`a[href="${UrlObj.pathname}"]`)[0].innerText;
+								result.question = document.querySelectorAll('div[role="tabpanel"] div div')[0].innerText;
+							}
+
 							// problemName: "872.↵Leaf-Similar Trees"
 							let problemArr = result.completeProblemName.split('\n');
-							result.problemName = problemArr[1];
+							result.problemName = problemArr[1] || problemArr[0].split('.')[1];
 							result.problemNumber = problemArr[0].split('.')[0];
-							result.question = document.querySelectorAll('div[role="tabpanel"] div div')[0].innerText;
 							return result;
 						},
 						{
@@ -43,7 +57,6 @@ module.exports = {
 					let problemData = await page.evaluate(
 						async ({ UrlObj }) => {
 							let result = {};
-							debugger;
 							result.difficulty = document.querySelectorAll('.difficulty-label')[0].innerText;
 							result.completeProblemName = document.querySelectorAll('.question-title .row h3')[0].innerText;
 							// problemName: "872. Leaf-Similar Trees"
