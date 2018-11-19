305ee8a93669e2e519990daad38b3fe9f0160a8d
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-11-19 23:34:10 +0530

    added directory reader

---
+++ Tools/toc-gen/index.js
@@ -0,0 +1,32 @@
+const { rootPath } = require('./package.json');
+const { lstatSync, readdirSync, stat, mkdirSync, writeFileSync, readFileSync } = require('fs');
+const { join, resolve } = require('path');
+const excludedDir = ['Tools'];
+const problemDirectory = resolve(__dirname, rootPath);
+const isDirectory = source => lstatSync(source).isDirectory();
+const getProblemDirectories = source =>
+	readdirSync(source)
+		.filter(name => name[0] !== '.')
+		.filter(name => !excludedDir.includes(name))
+		.filter(name => isDirectory(join(source, name)))
+		.map(name => ({ name, dir: join(source, name) }));
+
+const problemDirs = getProblemDirectories(problemDirectory);
+
+let allProblems = [];
+problemDirs.forEach(source => {
+	allProblems = allProblems.concat(
+		readdirSync(source.dir)
+			.filter(name => name[0] !== '.')
+			.filter(name => isDirectory(join(source.dir, name)))
+			.map(name => ({ name, difficulty: source.name, dir: join(source.dir, name) }))
+	);
+});
+
+allProblems = allProblems.map(t => {
+	let problemNumber = t.name.split(' ')[0];
+	let problemTitle = t.name.substr(t.name.indexOf(' ')).trim();
+	return Object.assign({}, t, { problemNumber, problemTitle });
+});
+
+console.log(allProblems);
