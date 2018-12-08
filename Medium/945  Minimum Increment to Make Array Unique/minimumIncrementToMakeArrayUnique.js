2efe6a2c172d0ea44210ea7c1a3976a95bb5e2ee
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-12-08 23:48:26 +0530

    added #945  Minimum Increment to Make Array Unique solution

---
+++ Medium/945  Minimum Increment to Make Array Unique/minimumIncrementToMakeArrayUnique.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var minIncrementForUnique = function (A) {
+    let visited = {};
+    let sum = 0;
+    for (let i = 0; i < A.length; i++) {
+        let n = A[i];
+        if (visited[n]) {
+            while (visited[n]) { sum++; n++ };
+            visited[n] = true;
+        }
+        else {
+            visited[n] = true;
+        }
+    }
+
+    return sum;
+};
