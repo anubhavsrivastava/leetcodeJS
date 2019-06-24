ce0b4227b2fb827ce588e63503f5e6a5650bcbf0
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-24 19:55:44 +0530

    added #27 solution

---
+++ Easy/27  Remove Element/removeElement.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+	let l = nums.length;
+	for (let i = 0; i < l; ) {
+		if (nums[i] === val) {
+			let subArr = nums.slice(i + 1);
+			nums.length = i;
+			nums.push(...subArr);
+			l--;
+		} else {
+			i++;
+		}
+	}
+
+	return nums.length;
+};
