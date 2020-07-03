07adb813d985f0368cb619a6d5699b0d5cd59c39
anubhavsrivastava <anubhav.srivastava00@gmail.com>
2020-07-03 20:08:47 +0200

    added 66 solution

--- Easy/66  Plus One/plusOne.js
+++ Easy/66  Plus One/plusOne.js
@@ -2,18 +2,28 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(d) {
-	if (!d) return d;
-	for (let i = d.length - 1; i >= 0; i--) {
-		let num = d[i] + 1;
+var plusOne = function (digits) {
+    let carry = 1;
+    for (let i = digits.length - 1; i >= 0; i--) {
 
-		if (num < 10) {
-			d[i] = num;
-			return d;
-		} else {
-			d[i] = num % 10;
-		}
-	}
-	d.unshift(1);
-	return d;
+        if (carry === 0) {
+            break;
+        }
+
+        if (digits[i] === 9) {
+            digits[i] = 0;
+            carry = 1;
+        }
+        else {
+            digits[i] = digits[i] + carry;
+            carry = 0;
+        };
+
+    }
+    if (carry === 1) {
+        digits[0] = 0;
+        digits = [1, ...digits]
+    }
+
+    return digits;
 };
